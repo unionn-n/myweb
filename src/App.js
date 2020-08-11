23c2a3bb0a1ee249cfca3363a3b16bab32a70c37
unionn-n <69508893+unionn-n@users.noreply.github.com>
2020-08-11 21:01:27 +0900

    Day1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button, Progress} from 'semantic-ui-react'
 function App() {
   return (
+    <div>
     <p> 안녕하세요! </p>
+    <Button Primary> hi </Button>
+    <Progress Percent = {33} indicating/>
+    </div>
   );
 }
 
